7c40467148505f051753bbf192a0a0b0b56d9566
Felix Lepoutre <felix@mrflow.co>
2018-07-23 14:53:20 +0200

    Created projects page, updated header and added location to server.js

--- server.js
+++ server.js
@@ -49,13 +49,20 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
 	res.render('about.hbs', {
-		pageTitle: "About page"
+		pageTitle: 'About page'
 	});
 });
 
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage: 'This is an error'
+	});
+});
+
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs',{
+		pageTitle: 'Projects page',
+		welcomeMessage: 'This page hosts all my projects'
 	});
 });
 
